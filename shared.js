3a451a5115f4e5c121cb6a0da46de66528055455
שלו שקיר <shalevshakir@gmail.com>
2025-10-14 23:17:32 +0300

    moved main functions to main.js

--- shared.js
+++ shared.js
@@ -1,29 +1,10 @@
 const backdrop = document.querySelector(".backdrop")
-const modal = document.querySelector(".modal")
-const buttons = document.querySelectorAll(".plan button")
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        backdrop.style.display = "block"
-        modal.style.display = "block"
-
-    })
-}
-
-const modal_action = document.querySelector(".modal__action--negative")
-
-modal_action.addEventListener("click", function () { modal.style.display = "none"; backdrop.style.display = "none" })
-
-backdrop.addEventListener("click", function () { backdrop.style.display = "none"; modal.style.display = "none" })
-
 const sidebar_toggle = document.querySelector(".toggle-button")
 const sidebar = document.querySelector(".mobile-nav");
 
 const open_sidebar = () => {
     backdrop.style.background = "transparent"
-
     sidebar.classList.add("toggle-button-on")
-    // sidebar.style.display = "block";
     backdrop.classList.add("toggle-button-on");
 
 
@@ -34,10 +15,10 @@ const close_sidebar = () => {
     backdrop.classList.remove("toggle-button-on")
     backdrop.style.background = "rgba(0, 0, 0, 0.5)"
 }
-
+//sidebar toggle off
 backdrop.addEventListener("click", close_sidebar)
 
-// sidebar toggle
+// sidebar toggle on
 sidebar_toggle.addEventListener("click", open_sidebar
 
 )
